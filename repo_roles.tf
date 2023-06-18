71f8333ec152a83f3e11236f39a9b247d735074c
michael <michael.jos.mccarthy@gmail.com>
2023-06-17 20:45:14 -0400

    Updated repo roles with inline policies w/ explicit denies

--- repo_roles.tf
+++ repo_roles.tf
@@ -1,5 +1,46 @@
 locals {
   github_username = "mmccarthy404"
+  this_repo       = "terraform-remote-state"
+}
+
+resource "aws_iam_role" "this_repo" {
+  name = "github-oidc-${local.this_repo}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "${aws_iam_openid_connect_provider.github.arn}"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          "StringEquals" = {
+            "token.actions.githubusercontent.com:aud" = "${var.github_oidc.client_id}"
+          }
+          "StringLike" = {
+            "token.actions.githubusercontent.com:sub" = "repo:${local.github_username}/${local.this_repo}:*"
+          }
+        }
+      }
+    ]
+  })
+
+  inline_policy {
+    name = "github-oidc-${local.this_repo}"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect   = "Allow"
+          Action   = "*"
+          Resource = "*"
+        }
+      ]
+    })
+  }
 }
 
 resource "aws_iam_role" "repos" {
@@ -10,7 +51,7 @@ resource "aws_iam_role" "repos" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow",
+        Effect = "Allow"
         Principal = {
           Federated = "${aws_iam_openid_connect_provider.github.arn}"
         }
@@ -18,7 +59,7 @@ resource "aws_iam_role" "repos" {
         Condition = {
           "StringEquals" = {
             "token.actions.githubusercontent.com:aud" = "${var.github_oidc.client_id}"
-          },
+          }
           "StringLike" = {
             "token.actions.githubusercontent.com:sub" = "repo:${local.github_username}/${each.key}:*"
           }
@@ -27,7 +68,41 @@ resource "aws_iam_role" "repos" {
     ]
   })
 
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AdministratorAccess"
-  ]
+  inline_policy {
+    name = "github-oidc-${each.key}"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect   = "Allow"
+          Action   = "*"
+          Resource = "*"
+        },
+        {
+          Effect = "Deny"
+          Action = "*"
+          Resource = [
+            "${aws_s3_bucket.state.arn}",
+            "${aws_s3_bucket.state.arn}/*"
+          ]
+          Condition = {
+            "StringNotEquals" = {
+              "s3:prefix" = "${each.key}"
+            }
+          }
+        },
+        {
+          Effect   = "Deny"
+          Action   = "*"
+          Resource = "${aws_dynamodb_table.state_lock.arn}"
+          Condition = {
+            "StringNotEquals" = {
+              "dynamodb:LeadingKeys" = "${aws_s3_bucket.state.arn}/${each.key}-md5"
+            }
+          }
+        }
+      ]
+    })
+  }
 }
